a679ec51d137f68feecc5028cda81ca46bdde588
jecordjotse <jeromecordjotse@gmail.com>
2020-05-10 03:23:42 +0000

    final question finished

--- rankall.R
+++ rankall.R
@@ -1,4 +1,4 @@
-rankhospital <- function(state, outcome, num = "best") {
+rankall <- function(outcome, num = "best") {
   ## Read outcome data
   
   # reading outcomes into outcome variable
@@ -12,12 +12,12 @@ rankhospital <- function(state, outcome, num = "best") {
       data[,23] <- suppressWarnings(as.numeric(data[, 23]))
       # extract columns 11, 17 and 23 together with Hospital name columns (col 2) and group by State (col 7)
       tempData <- na.omit(data[c(2,7,11,17,23)])
-      outcomeByState <- split(tempData,tempData$State)
-      
-      
+      #outcomeByState <- split(tempData,tempData$State)
+      f <- factor(data$State)
+      fLevels <- names(table(f))
   ## Check that state and outcome are valid
       
-      st <- state %in% names(outcomeByState)  # check if state value is valid (exist if list of present states)
+      #st <- state %in% names(outcomeByState)  # check if state value is valid (exist if list of present states)
       switch ( outcome,                       # Used Switch to detemine col from outcome
                "heart attack" = col <- 3,
                "heart failure" = col <- 4,
@@ -25,28 +25,33 @@ rankhospital <- function(state, outcome, num = "best") {
                "default" = col <- 0
       )
       col <- if(col==0) FALSE else col        # check if outcome is either "heart attack",  "heart failure", or "pneumonia"
+      
+      
+      
+      if(!col) stop("invalid outcome",call. = !col)
+      tempData <- tempData[order(tempData[,col]),]
+      outcomeByState <- split(tempData,tempData$State)
+      
+      
+      ## For each state, find the hospital of the gtempTry.tempTry.tempTry.tempTry.tempTry.tempTry.tempTry.tempTry.tempTry.tempTry.tempTry.iven rank
+      
+      
+      rank <- if(num %in% c(1:len)) num else switch(num, "best" = 1,"worst" = len, 0)
+      outcomeByState <- lapply(outcomeByState, function(x){len <- nrow(x);write.csv(x,'data/b4Order.csv');x$rank <- c(1:len);write.csv(x,'data/aftOrder.csv');x})
 
       
-  ## Return hospital name in that state with the given rank
-  ## 30-day death rate
       
-      if(!st) stop("invalid state",call. = !st)
-      if(!col) stop("invalid outcome",call. = !col)
-      stateData <- outcomeByState[[state]]
-      stateData <- stateData[order(stateData[,col]),]
-      len <- nrow(stateData)
-      #print(num)
-      #print(len)
-      rank <- if(num %in% c(1:len)) num else switch(num, "best" = 1,"worst" = len, 0)
-      row.names(stateData) <- 1:len
-      #print(rank)
-      #print(stateData)
-      return(if(!is.null(rank)) stateData[rank,][,1] else NA)
-      
-    }else{
-      print("Error: datafile \"outcome-of-care-measures.csv\" does not exist")
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+      #outcomeByState
+      output = character(0)
+      for(i in fLevels){
+        temp <- outcomeByState[[i]][outcomeByState[[i]]['rank'] == rank,1]
+        if(length(temp)==0) temp <- NA
+        output <- c(output,temp)
+      }
+      data.frame(output,fLevels)
     }
-  }else{
-    print("Error: Not in ProgrammingAssignment3 dir or data/ dir missing from ProgrammingAssignment3/")
   }
 }
+
